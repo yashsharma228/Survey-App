df646201bf0259ddfda1d859992e0c8c938c2a46
https://github.com/yashsharma228 <yashsharma4841@gmail.com>
2025-04-23 19:41:31 +0530

    auth changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import WelcomeScreen from "./components/WelcomeScreen";
 import SurveyScreen from "./components/SurveyScreen";
 import ThankYouScreen from "./components/ThankYouScreen";
@@ -7,15 +12,47 @@ import { SurveyProvider } from "./context/SurveyContext";
 import "./App.css";
 
 const App = () => {
+  const { user, loginWithRedirect, isAuthenticated, logout, isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <SurveyProvider>
+      <>
+      {!isAuthenticated ? (
+  <div className="auth-wrapper">
+    <div className="auth-box">
+      <h2>Welcome to the Survey App</h2>
+      <p>Please login to continue</p>
+      <button className="auth-btn" onClick={() => loginWithRedirect()}>
+        Login to Start Survey
+      </button>
+    </div>
+  </div>
+) : (
+  <div className="authenticated-container">
+    <div className="user-bar">
+    <div className="user-name">👋 Hello, {user.name}</div>
+      <button className="logout-btn" onClick={() => logout({ returnTo: window.location.origin })}>
+        Logout
+      </button>
+    </div>
+
+    <div className="auth-content">
       <Router>
         <Routes>
           <Route path="/" element={<WelcomeScreen />} />
-          <Route path="/survey" element={<SurveyScreen />} />
+          <Route path="/survey" element={<SurveyScreen />} /> 
           <Route path="/thankyou" element={<ThankYouScreen />} />
         </Routes>
       </Router>
+    </div>
+  </div>
+)}
+
+
+
+      </>
     </SurveyProvider>
   );
 };
